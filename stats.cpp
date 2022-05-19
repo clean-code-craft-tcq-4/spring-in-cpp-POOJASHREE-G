ef96dc12b60f7fffd8d66c95ba0537292fdf492f
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-05-19 18:58:31 +0530

    ComputeStatistics function implementation
    ComputeStatistics function implementation...

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,19 @@
 #include "stats.h"
+#include <algorithm>
+#include <numeric>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
     //Implement statistics here
+
+	float maxElement = *std::max_element(vect.begin(), vect.end());
+	float minElement = *std::min_element(vect.begin(), vect.end());
+	float average = accumulate( vect.begin(), vect.end(), 0.0/ vect.size());
+
+	Statistics::Stats s;
+
+	s.average = average;
+	s.max = maxElement;
+	s.min = minElement;
+
+	return s;
 }

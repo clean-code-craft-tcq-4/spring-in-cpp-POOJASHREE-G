09961fe962ad49082f9fc07b81db28cfc174187c
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-05-19 17:12:33 +0530

    Added Class Stats in stats.h
    Class addition names Stats

--- stats.h
+++ stats.h
@@ -1,5 +1,13 @@
 #include <vector>
 
 namespace Statistics {
+    
+    class Stats
+    {
+        double average;
+        float max;
+        float min;
+    };
+    
     Stats ComputeStatistics(const std::vector<___>& );
 }

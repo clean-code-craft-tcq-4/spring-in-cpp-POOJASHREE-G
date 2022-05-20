b23b9740a3db76c5eb89f6b656f436b1d4e50a94
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-05-20 11:43:10 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,16 +4,21 @@
 
 Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
     //Implement statistics here
+        Statistics::Stats s;
+	
+	if (!(vect.empty()))
+	{
+		float maxElement = *std::max_element(vect.begin(), vect.end());
+		float minElement = *std::min_element(vect.begin(), vect.end());
+		float average = accumulate(vect.begin(), vect.end(), 0.0) / vect.size();
 
-	float maxElement = *std::max_element(vect.begin(), vect.end());
-	float minElement = *std::min_element(vect.begin(), vect.end());
-	float average = accumulate(vect.begin(), vect.end(), 0.0) / vect.size();
 
-	Statistics::Stats s;
 
-	s.average = average;
-	s.max = maxElement;
-	s.min = minElement;
+		s.average = average;
+		s.max = maxElement;
+		s.min = minElement;
+		
+	}
 
 	return s;
 }

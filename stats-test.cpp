d9149f3b535db39ac8e93ca55c3347ddd41bd0eb
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-05-20 11:17:42 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -20,7 +20,7 @@ TEST_CASE("average is NaN for empty array") {
     
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
-    REQUIRE(std::isnan(computedStats.average));
+    REQUIRE(std::isnan(computedStats.average) == NaN);
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {

2fda7255f8966bcd1866488e1e8cd89045e60214
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-05-20 10:30:55 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -11,4 +11,68 @@ namespace Statistics {
     };
     
     Stats ComputeStatistics(const std::vector<float>& );
+    
+    class IAlerter
+	{
+	public:
+		virtual void Update() = 0; 
+	};
+
+	class EmailAlert : public IAlerter
+	{
+	public:
+		bool emailSent;
+
+		EmailAlert()
+			 : emailSent(false) { };
+
+		void Update()
+		{
+			emailSent = true;
+		}
+
+	};
+
+	class LEDAlert : public IAlerter
+	{
+	public:
+		bool ledGlows;
+
+		LEDAlert()
+			: ledGlows(false) { };
+
+
+		void Update()
+		{
+			ledGlows = true;
+		}
+
+	};
+
+	class StatsAlerter
+	{
+	public:
+
+		std::vector<IAlerter*> Alrts;
+    
+        float maxthres;
+
+		StatsAlerter(const float maxthresold, std::vector<IAlerter*> alerts)
+		{
+			maxthres = maxthresold;
+			Alrts = alerts;
+		}
+
+		void checkAndAlert(const std::vector<float>& vect)
+		{
+			auto ComputedStats = Statistics::ComputeStatistics(vect);
+			if ( ComputedStats.max > maxthres )
+			{
+				for (int i = 0; i < Alrts.size(); ++i)
+				{
+					Alrts[i]->Update();
+				}
+			}
+		};
+	};
 }
